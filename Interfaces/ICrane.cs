d61136682bdddb4fb106a72f1e75f213f121a20d
bastelfreak <tim@bastelfreak.de>
2018-01-13 15:18:30 +0100

    reformat ICrane

--- Interfaces/ICrane.cs
+++ Interfaces/ICrane.cs
@@ -87,26 +87,32 @@ namespace DemoOpenGLBasicsCS.interfaces
             GL.glColor3f(colorred, colorgreen, colorblue);
 
             GL.glTranslated(0.0, 0.0, 0.0);
-            matrix.Startvektor = new System.Windows.Media.Media3D.Vector3D(0.0, 0.0, -6.0);
+            matrix.Startvektor = new System.Windows.Media.Media3D.Vector3D(0.0, 0.0, 0.0);
 
             GL.glRotated(-90, 1, 0, 0);
+            //GL.glRo
             GL.glRotated(drehwinkel, 0, 0, 1);
-            //matrix.Matrixstart = System.Windows.Media.Media3D.Matrix3D.Multiply(new System.Windows.Media.Media3D.Matrix3D(1, 0, 0, 0, 0, Math.Cos(-90), -(Math.Sin(-90)), 0, 0, Math.Sin(-90), Math.Cos(-90), 0, 0, 0, 0, 1),
-            //                                                                    new System.Windows.Media.Media3D.Matrix3D(Math.Cos(drehwinkel), -(Math.Sin(drehwinkel)), 0, 0, Math.Sin(drehwinkel), Math.Cos(drehwinkel), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1));
             matrix.Matrixstart = System.Windows.Media.Media3D.Matrix3D.Multiply(
                 new System.Windows.Media.Media3D.Matrix3D(
-                    1, 0, 0, 0, 0, Math.Cos(Degree2Radiant(-90)),
-                    -(Math.Sin(Degree2Radiant(-90))), 0, 0, Math.Sin(Degree2Radiant(-90)), Math.Cos(Degree2Radiant(-90)), 0, 0, 0, 0, 1),
-                                                                                new System.Windows.Media.Media3D.Matrix3D(Math.Cos(Degree2Radiant(drehwinkel)), -(Math.Sin(Degree2Radiant(drehwinkel))), 0, 0, Math.Sin(Degree2Radiant(drehwinkel)), Math.Cos(Degree2Radiant(drehwinkel)), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1));
-
-            matrix.Turmzuausleger = System.Windows.Media.Media3D.Matrix3D.Multiply(new System.Windows.Media.Media3D.Matrix3D(Math.Cos(Degree2Radiant(90)), 0, Math.Sin(Degree2Radiant(90)), 0, 0, 1, 0, 0, -(Math.Sin(Degree2Radiant(90))), 0, Math.Cos(Degree2Radiant(90)), 0, 0, 0, 0, 1),
-                                                                                   new System.Windows.Media.Media3D.Matrix3D(Math.Cos(Degree2Radiant(90)), -(Math.Sin(Degree2Radiant(90))), 0, 0, Math.Sin(Degree2Radiant(90)), Math.Cos(Degree2Radiant(90)), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1));
-
+                    1, 0, 0, 0,
+                    0, Math.Cos(Degree2Radiant(-90)), -(Math.Sin(Degree2Radiant(-90))), 0,
+                    0, Math.Sin(Degree2Radiant(-90)), Math.Cos(Degree2Radiant(-90)), 0,
+                    0, 0, 0, 1
+                ),
+                new System.Windows.Media.Media3D.Matrix3D(
+                    Math.Cos(Degree2Radiant(drehwinkel)), -(Math.Sin(Degree2Radiant(drehwinkel))), 0, 0,
+                    Math.Sin(Degree2Radiant(drehwinkel)), Math.Cos(Degree2Radiant(drehwinkel)), 0, 0,
+                    0, 0, 1, 0,
+                    0, 0, 0, 1
+                )
+            );
 
             turm = new CylinderPart(3);
+            //GL.glRotated(-90, 1, 0, 0);
             GLU.gluCylinder(turm.element, 0.2, 0.2, turm.length, 4, 10);
             GLU.gluQuadricDrawStyle(turm.element, style);
             matrix.Matrixturm = new System.Windows.Media.Media3D.Vector3D(0.0, 0.0, turm.length);
+            
 
             ausleger = new CylinderPart(2);
             GL.glTranslated(0.0, 0.0, turm.length - 0.2f);
@@ -115,8 +121,8 @@ namespace DemoOpenGLBasicsCS.interfaces
             GLU.gluCylinder(ausleger.element, 0.2, 0.2, ausleger.length, 3, 10);
             GLU.gluQuadricDrawStyle(ausleger.element, style);
             matrix.Matrixausleger = new System.Windows.Media.Media3D.Vector3D(0.0, 0.0, turm.length - 0.2f);
-            matrix.Turmzuausleger = System.Windows.Media.Media3D.Matrix3D.Multiply(new System.Windows.Media.Media3D.Matrix3D(Math.Cos(90), 0, Math.Sin(90), 0, 0, 1, 0, 0, -(Math.Sin(90)), 0, Math.Cos(90), 0, 0, 0, 0, 1),
-                                                                                   new System.Windows.Media.Media3D.Matrix3D(Math.Cos(90), -(Math.Sin(90)), 0, 0, Math.Sin(90), Math.Cos(90), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1));
+            matrix.Turmzuausleger = System.Windows.Media.Media3D.Matrix3D.Multiply(new System.Windows.Media.Media3D.Matrix3D(Math.Cos(Degree2Radiant(90)), 0, Math.Sin(Degree2Radiant(90)), 0, 0, 1, 0, 0, -(Math.Sin(Degree2Radiant(90))), 0, Math.Cos(Degree2Radiant(90)), 0, 0, 0, 0, 1),
+                                                                                   new System.Windows.Media.Media3D.Matrix3D(Math.Cos(Degree2Radiant(90)), -(Math.Sin(Degree2Radiant(90))), 0, 0, Math.Sin(Degree2Radiant(90)), Math.Cos(Degree2Radiant(90)), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1));
 
             seil = new CylinderPart(seillaenge);
             GL.glTranslated(0.0, 0.0, Seilposition);
@@ -125,8 +131,8 @@ namespace DemoOpenGLBasicsCS.interfaces
             GLU.gluCylinder(seil.element, 0.01, 0.01, seil.length, 20, 10);
             GLU.gluQuadricDrawStyle(seil.element, style);
             matrix.Matrixseil = new System.Windows.Media.Media3D.Vector3D(0.0, 0.0, Seilposition);
-            matrix.Auslegerzuseil = System.Windows.Media.Media3D.Matrix3D.Multiply(new System.Windows.Media.Media3D.Matrix3D(Math.Cos(90), 0, Math.Sin(90), 0, 0, 1, 0, 0, -(Math.Sin(90)), 0, Math.Cos(90), 0, 0, 0, 0, 1),
-                                                                                    new System.Windows.Media.Media3D.Matrix3D(1, 0, 0, 0, 0, Math.Cos(90), -(Math.Sin(90)), 0, 0, Math.Sin(90), Math.Cos(90), 0, 0, 0, 0, 1));
+            matrix.Auslegerzuseil = System.Windows.Media.Media3D.Matrix3D.Multiply(new System.Windows.Media.Media3D.Matrix3D(Math.Cos(Degree2Radiant(90)), 0, Math.Sin(Degree2Radiant(90)), 0, 0, 1, 0, 0, -(Math.Sin(Degree2Radiant(90))), 0, Math.Cos(Degree2Radiant(90)), 0, 0, 0, 0, 1),                                                                                    new System.Windows.Media.Media3D.Matrix3D(1, 0, 0, 0, 0, Math.Cos(Degree2Radiant(90)), -(Math.Sin(Degree2Radiant(90))), 0, 0, Math.Sin(Degree2Radiant(90)), Math.Cos(Degree2Radiant(90)), 0, 0, 0, 0, 1));
+
             matrix.SeilVector = new System.Windows.Media.Media3D.Vector3D(0.0, 0.0, Seillaenge);
 
             GL.glTranslated(0.0, 0.0, Seillaenge);
