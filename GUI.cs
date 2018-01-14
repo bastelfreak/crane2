63310e067c90055db836310baf801e168a5163b8
bastelfreak <tim@bastelfreak.de>
2018-01-14 10:28:54 +0100

    correct swapped Z/Y

--- GUI.cs
+++ GUI.cs
@@ -32,8 +32,8 @@ namespace DemoOpenGLBasicsCS
             tbx_xz_movement.Text = oglView.Kran1.MovementfactorXZ.ToString();
             tbx_y_movement.Text = oglView.Kran1.MovementfactorY.ToString();
             tbx_tri_x.Text = Convert.ToString(Math.Cos(Math.PI * oglView.Kran1.Drehwinkel / 180) * oglView.Kran1.Seilposition);
-            tbx_tri_y.Text = Convert.ToString(Math.Sin(Math.PI * oglView.Kran1.Drehwinkel / 180) * oglView.Kran1.Seilposition);
-            tbx_tri_z.Text = Convert.ToString(oglView.Kran1.Towerlength - oglView.Kran1.Seillaenge);
+            tbx_tri_y.Text = Convert.ToString(oglView.Kran1.Towerlength - oglView.Kran1.Seillaenge);
+            tbx_tri_z.Text = Convert.ToString(Math.Sin(Math.PI * oglView.Kran1.Drehwinkel / 180) * oglView.Kran1.Seilposition);
         }
 
         private void Movement(interfaces.IMovement b)
